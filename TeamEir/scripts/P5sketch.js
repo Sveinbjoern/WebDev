4f0aa366ffee0fb9ccc2967b1b50fb673d791028
Sveinbjoern <sveinbjoern@gmail.com>
2021-09-10 12:13:17 +0200

    .

--- TeamEir/scripts/P5sketch.js
+++ TeamEir/scripts/P5sketch.js
@@ -10,7 +10,9 @@ let keyCodes = {
 
 let update = false;
 let tabVal = {
-	tabBottom: -10, //set in setup
+	amount: 4,
+
+	tabBottom: -5, //set in setup
 	tabHeight: -50, //set in setup
 	tabWidth: 220,
 	tabNarrow: 30,
@@ -27,10 +29,32 @@ let tabVal = {
 	tabColor: [], //set in setup
 	tabStarts: [],//set in setup
 
+	setup: function (){
+		tabVal.tabBottom += height;
+		tabVal.tabHeight = tabVal.tabHeight + tabVal.tabBottom;
+		tabVal.distance += tabVal.tabWidth;
+		tabVal.tabColor = [colors.blue, colors.indigo, colors.satinSheenGold ];
+		for (let i = 0; i < tabVal.amount; i++)
+		{
+			tabVal.tabStarts.push(tabVal.start + tabVal.distance * i);
+		}
+		
+	}
 
 } 
 
-let colors = {}; // made in setup
+let colors = {
+
+	setup: function(){
+		colors.blue = "#5139e4"
+		colors.indigo = "#4b0082"
+		colors.satinSheenGold = "#ad9223";
+		colors.black = "#0b090A"
+		colors.white = "#e3ddde"
+	}
+
+	
+}; // made in setup
 
 
 const soundeffects = {};
@@ -47,29 +71,21 @@ function setup() {
 	console.log("setup")
 	setAttributes('antialias', true)
 	//basic p5 settings:
-	frameRate(30);
+	frameRate(25);
 
 	//create a canvas to fill the content div from index.html
 	canvasContainer = select('#p5canvas');
 	var c = createCanvas(canvasContainer.size().width, canvasContainer.size().height);
 	c.parent('p5canvas');
 
-	colors.blue = "#5139e4"
-	colors.indigo = "#4b0082"
-	colors.satinSheenGold = "#ad9223";
-	colors.black = "#0b090A"
-	colors.white = "#e3ddde"
-	background(255);
 	
+	// background(255);
 	
-	tabVal.tabBottom += height;
-	tabVal.tabHeight = tabVal.tabHeight + tabVal.tabBottom;
-	tabVal.distance += tabVal.tabWidth;
+	colors.setup()
+	
+	tabVal.setup();
 
-
-	tabVal.tabColor = [colors.indigo, colors.satinSheenGold, colors.blue]
-
-	tabVal.tabStarts = [tabVal.start, tabVal.start + tabVal.distance,tabVal.start + tabVal.distance*2 ]
+	
 	//create the drawManager
 	drawTabs();
 
@@ -91,53 +107,58 @@ function draw() {
 }
 
 
-function fitToScreen(){
- console.log("fitToScreen");
- 
- 
- canvasContainer = select('#p5canvas');
- var c = createCanvas(canvasContainer.size().width, canvasContainer.size().height);
- c.parent('p5canvas');
- 
-}
 
-function drawTabs(){
-	strokeWeight(3);
-	stroke(colors.black)
-	drawTab(tabVal.tabStarts[0], tabVal.tabColor[0]);
-	drawTab(tabVal.tabStarts[1], tabVal.tabColor[1]);
-	drawTab(tabVal.tabStarts[2], tabVal.tabColor[2]);
-	
-	let tabHeight = tabVal.tabBottom -1;
-	line(0,tabHeight, tabVal.tabStarts[currentTab], tabHeight);
-	line(tabVal.tabStarts[currentTab]+ tabVal.tabWidth, tabHeight, width, tabHeight);
 
-	
-	noStroke();
-	fill(tabVal.tabColor[currentTab])
-	rect(0,tabVal.tabBottom , width, tabVal.tabBottom)
-}
 
-function drawTab(startingX, color){
-	fill(color);
-	beginShape();
-	vertex(startingX, tabVal.tabBottom)
-	vertex(startingX + tabVal.tabNarrow, tabVal.tabHeight)
-	vertex(startingX + tabVal.tabWidth - tabVal.tabNarrow, tabVal.tabHeight)
-	vertex(startingX + tabVal.tabWidth, tabVal.tabBottom)
-	endShape(); 
-	// console.log("logging drawTab", startingX, tabVal.tabBottom, tabVal.tabHeight )
-}
 
 function keyPressed(){
-	if (keyCode = keyCodes.R)
+	if (keyCode === keyCodes.R)
 	{
-	
-		let elem = document.getElementById("languageEN")
-		// console.log(elem.checked)
-		elem.checked = !elem.checked;
-		// console.log(elem.value)
+		// console.log("keycode R")
+		// let elem = document.getElementById("languageEN")
+		// // console.log(elem.checked)
+		// elem.checked = !elem.checked;
+		// // console.log(elem.value)
 
+		let next = false;
+		let changed = false;
+		elem = document.getElementById("cat_type_all")
+		// console.log(elem.checked)
+		if (elem.checked) {next = true}
+		elem = document.getElementById("cat_type_tegneserie")
+		if (next)
+		{
+			elem.checked = true;
+			changed = true;
+			next = false;
+			console.log(elem)
+		} else if(elem.checked) {next = true}
+
+		elem = document.getElementById("cat_type_husflid")
+		if (next)
+		{
+			elem.checked = true;
+			changed = true
+			next = false
+			console.log(elem)
+		} else if(elem.checked) {next = true}
+		elem = document.getElementById("cat_type_utfoldelse")
+		if (next)
+		{
+			elem.checked = true;
+			changed = true
+			next =false
+			console.log(elem)
+		} else if(elem.checked) {next = true}
+
+		if (next || !changed)
+		{
+			elem = document.getElementById("cat_type_all")
+			elem.checked = true;
+			console.log(elem)
+		}
+
+	
 		// elem = document.getElementsByClassName("English")
 
 		// elem.forEach(element => {
@@ -152,7 +173,7 @@ function mousePressed(){
 	
 	if (mouseY > tabVal.tabHeight && mouseY < tabVal.tabBottom)
 	{
-		console.log("correct height")
+		// console.log("correct height")
 		if (mouseX > tabVal.tabStarts[0]+ tabVal.tabNarrow && mouseX < tabVal.tabStarts[0] + tabVal.tabWidth - tabVal.tabNarrow)
 		{
 			currentTab = 0;
@@ -164,6 +185,10 @@ function mousePressed(){
 		} else if (mouseX > tabVal.tabStarts[2]+ tabVal.tabNarrow && mouseX < tabVal.tabStarts[2] + tabVal.tabWidth - tabVal.tabNarrow)
 		{
 			currentTab = 2;
+			update = true;
+		} else if (mouseX > tabVal.tabStarts[3]+ tabVal.tabNarrow && mouseX < tabVal.tabStarts[3] + tabVal.tabWidth - tabVal.tabNarrow)
+		{
+			currentTab = 3;
 			update = true;
 		}
 	}
---
+++ TeamEir/scripts/helperFunctions.js
@@ -0,0 +1,40 @@
+function drawTabs(){
+	strokeWeight(3);
+	stroke(colors.black)
+    for (let i = 0; i < tabVal.amount; i ++)
+    {
+        // console.log("amount of tabs + starts + color", tabVal.tabStarts[0],  tabVal.tabColor[ i%3] )
+        drawTab(tabVal.tabStarts[i], tabVal.tabColor[ i%3])
+    }
+	
+	let tabHeight = tabVal.tabBottom -1;
+	line(0,tabHeight, tabVal.tabStarts[currentTab], tabHeight);
+	line(tabVal.tabStarts[currentTab]+ tabVal.tabWidth, tabHeight, width, tabHeight);
+
+	
+	noStroke();
+	fill(tabVal.tabColor[currentTab%3])
+	rect(0,tabVal.tabBottom , width, tabVal.tabBottom)
+}
+
+function drawTab(startingX, color){
+	fill(color);
+	beginShape();
+	vertex(startingX, tabVal.tabBottom)
+	vertex(startingX + tabVal.tabNarrow, tabVal.tabHeight)
+	vertex(startingX + tabVal.tabWidth - tabVal.tabNarrow, tabVal.tabHeight)
+	vertex(startingX + tabVal.tabWidth, tabVal.tabBottom)
+	endShape(); 
+	// console.log("logging drawTab", startingX, tabVal.tabBottom, tabVal.tabHeight )
+}
+
+function fitToScreen(){
+    console.log("fitToScreen");
+    
+    
+    canvasContainer = select('#p5canvas');
+    var c = createCanvas(canvasContainer.size().width, canvasContainer.size().height);
+    c.parent('p5canvas');
+    update = true;
+    
+   }
